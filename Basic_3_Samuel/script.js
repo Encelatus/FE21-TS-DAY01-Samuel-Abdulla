0eaea1b8240590d1e345aea1d37781e163c7ddef
Samuel Tenschert <samuel.tenschert@gmail.com>
2021-03-17 14:08:03 +0100

    done basic 3 samuel

---
+++ Basic_3_Samuel/script.js
@@ -0,0 +1,10 @@
+var nameList = ["Samuel", "Mario", "Abdulla", "Loveleen", "Agnes", "Faris", "Andrea"];
+for (var i = 0; i < nameList.length; i++) {
+    document.write(i);
+}
+for (var i = 0; i < nameList.length; i++) {
+    document.write("<br>" + nameList[i]);
+}
+for (var i = 0; i < nameList.length; i++) {
+    document.write("<br>" + [i] + nameList[i]);
+}
---
+++ Basic_3_Samuel/script.ts
@@ -0,0 +1,15 @@
+let nameList: Array<string> = ["Samuel", "Mario", "Abdulla", "Loveleen", "Agnes", "Faris", "Andrea"]
+
+for (let i = 0; i < nameList.length; i++) {
+    document.write(i);
+
+}
+for (let i = 0; i < nameList.length; i++) {
+    document.write(`<br>`+ nameList[i]);
+
+}
+
+for (let i = 0; i < nameList.length; i++) {
+    document.write(`<br>`+[i]+nameList[i]);
+
+}
