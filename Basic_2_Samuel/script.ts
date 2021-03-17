c06f9bc425580b6b5a01348e0e7eddd5aee10cbe
Samuel Tenschert <samuel.tenschert@gmail.com>
2021-03-17 13:29:46 +0100

    .

--- Basic_2_Samuel/script.ts
+++ Basic_2_Samuel/script.ts
@@ -1,1 +1,7 @@
-var name : 
+var myNames = [{"fName": "Samuel", "lName": " Tenschert"}];
+document.write(myNames[0].fName,myNames[0].lName);
+
+
+myNames.forEach(function (value) {
+  console.log(myNames[0].fName);
+});
