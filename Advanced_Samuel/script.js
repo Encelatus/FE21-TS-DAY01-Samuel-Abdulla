1a9eb3c7336e22585e8acaa19b60627cb7b9d626
Samuel Tenschert <samuel.tenschert@gmail.com>
2021-03-17 15:03:51 +0100

    advanced starting

---
+++ Advanced_Samuel/script.js
@@ -0,0 +1,5 @@
+var startersMenu = ["Toast", "Soup"];
+var mainMenu = ["Spaghetti", "Schnitzel"];
+var dessertMenu = ["Ice Cream", "Tiramisu"];
+for (var i = 0; i < 3; i++) {
+}
---
+++ Advanced_Samuel/script.ts
@@ -0,0 +1,7 @@
+var startersMenu: Array<string> = ["Toast", "Soup"];
+var mainMenu: Array<string> = ["Spaghetti", "Schnitzel"];
+var dessertMenu: Array<string> = ["Ice Cream", "Tiramisu"];
+
+for (let i = 0; i < 3; i++) {
+    
+}
