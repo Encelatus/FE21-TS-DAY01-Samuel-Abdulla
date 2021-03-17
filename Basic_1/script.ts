dcda6f73e3d81fa9ffeb5303a4c7b4c63797c255
Samuel Tenschert <samuel.tenschert@gmail.com>
2021-03-17 11:43:42 +0100

    basic1solution

--- Basic_1/script.ts
+++ Basic_1/script.ts
@@ -4,17 +4,17 @@
     
 // });
 
-var  num:number[]=[1,2,3,4,5,6,7,8,9,10];
+// var  num:number[]=[1,2,3,4,5,6,7,8,9,10];
 
-for (var i = num[0]; i <=num.length; i++) {
+// for (var i = num[0]; i <=num.length; i++) {
 
    
-    for(var j=1;j<=10;j++){
+//     for(var j=1;j<=10;j++){
 
-        console.log(`${i} X ${j} = ${i*j}`);
-    }
+//         console.log(`${i} X ${j} = ${i*j}`);
+//     }
 
-}
+// }
 
 //  for (const index in num) {
 //         if (Object.prototype.hasOwnProperty.call(num, index)) {
@@ -22,3 +22,10 @@ for (var i = num[0]; i <=num.length; i++) {
             
 //         }
 //     }
+document.write("<table border='1px'>");
+for (var i = 1; i <= 10; i++){
+    for (var j = 1; j <= 10; j++){
+        document.write(`<td style='width:30px; height:50px;'>${i}X${j}=${i*j}</td>`);
+    }
+    document.write("</tr>");
+}
